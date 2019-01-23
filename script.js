70f043c33f678367a6febd72898b8384682d1ee2
ianblaylock <ldad@ls2-alr.serfc.noaa.gov>
2019-01-23 02:56:53 +0000

    added the video buttons!

--- script.js
+++ script.js
@@ -43,3 +43,32 @@ function initializeMenus() {
 initializeMenus();
 selectMenuItemInDict(lvl1Dict,"aboutMeMenuItem");
 selectMenuItemInDict(softwareDict,"obsMonitorMenuItem");
+
+//initialize videos
+function initializeVideos() {
+    let michaelVideo = document.getElementById("michaelVideo");
+    let florenceVideo = document.getElementById("florenceVideo");
+    michaelVideo.style.display = "block";
+    let michaelButton = document.getElementById("michaelButton");
+    let florenceButton = document.getElementById("florenceButton");
+    
+    michaelButton
+	.addEventListener("click",
+			  function() {michaelButton.style.borderColor = "rgb(236,88,0)";
+				      florenceButton.style.borderColor = "#ffffff";
+				      florenceVideo.style.display = "none";
+				      michaelVideo.style.display = "block";
+				      
+				     });
+
+    florenceButton
+	.addEventListener("click",
+			  function() {florenceButton.style.borderColor = "rgb(236,88,0)";
+				      michaelButton.style.borderColor = "#ffffff";
+				      michaelVideo.style.display = "none";
+				      florenceVideo.style.display = "block";});
+				      
+}
+
+initializeVideos();
+
