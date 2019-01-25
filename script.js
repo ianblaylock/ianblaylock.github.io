83c6f641dae65ba8267d80e1a361e6e3363bc530
ianblaylock <ldad@ls2-alr.serfc.noaa.gov>
2019-01-25 05:57:29 +0000

    overhauled a whole lot

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-//set up the menu functionality 
-
-var lvl1Dict = {"aboutMeMenuItem": "aboutMe",
-		"softwareProjectsMenuItem": "softwareProjects",
-		"skillsMenuItem": "skillsList"};
-
-var softwareDict = {"obsMonitorMenuItem": "obsMonitor",
-		    "gageScatterplotMenuItem": "gageScatterplot",
-		    "hydroDataViewerMenuItem": "hydroDataViewer",
-		    "quickHydroBriefMenuItem": "quickHydroBrief"};
-
-function setTextColorByPropertyValue(elemId,colorTag) {
-    document.getElementById(elemId).style.color =
-	getComputedStyle(document.body).getPropertyValue(colorTag);
-}
-
-function setElementDisplayById(elemId,displayValue) {
-    document.getElementById(elemId).style.display = displayValue;
-}
-
-function selectMenuItemInDict(dict,dictKey) {
-    for (var key in dict) {
-	let menuElemId = key;
-	let pageElemId = dict[key];
-	setTextColorByPropertyValue(menuElemId,(menuElemId === dictKey) ? '--menu-selection-text-color' : '--main-text-color');
-	setElementDisplayById(pageElemId,(menuElemId === dictKey) ? "block" : "none");	
-    }
-}
-
-function initializeMenus() {
-    let dictList = [lvl1Dict, softwareDict];
-    for (var i = 0; i < dictList.length; i++) {
-	let localDict = dictList[i];
-	for (var key in localDict) {
-	    let localKey = key;
-	    let value = localDict[localKey];
-	    document.getElementById(localKey)
-		.addEventListener("click", function() {selectMenuItemInDict(localDict,localKey);});
-	}
-    }
-}
-
-initializeMenus();
-selectMenuItemInDict(lvl1Dict,"aboutMeMenuItem");
-selectMenuItemInDict(softwareDict,"obsMonitorMenuItem");
-
-//initialize videos
 function initializeVideos() {
     let michaelVideo = document.getElementById("michaelVideo");
     let florenceVideo = document.getElementById("florenceVideo");
@@ -71,4 +24,3 @@ function initializeVideos() {
 }
 
 initializeVideos();
-
