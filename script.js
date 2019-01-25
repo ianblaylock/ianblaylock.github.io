073a066a1f6b6b63e3b3855f605a819d9441c5a0
ianblaylock <catfish.deity@gmail.com>
2019-01-25 08:36:56 -0800

    changes from 25 january 2019

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+function initializeMenus() {
+    
+    function hideAllContentContainers() {
+	let elems = document.getElementsByClassName("contentContainer");
+	for (var i = 0; i < elems.length; i++) {
+	    elems[i].style.display = "none";}
+    }
+
+    function hideAllSoftwareProjectContainers() {
+	let elems = document.getElementsByClassName("softwareProjectContainer");
+	for (var i = 0; i < elems.length; i++) {
+	    elems[i].style.display = "none";}
+    }
+    //menu1 buttons
+    let aboutMeBtn = document.getElementById("aboutMeMenuItem");
+    let projectsBtn = document.getElementById("softwareProjectsMenuItem");
+    let skillsBtn = document.getElementById("skillsMenuItem");
+    //menu1 sections
+    let bio = document.getElementById("bio");
+    let projects = document.getElementById("projects");
+    let skills = document.getElementById("skills");
+
+    //menu2 buttons
+    let obsMonitorBtn = document.getElementById("obsMonitorMenuItem");
+    let gageScatterplotBtn = document.getElementById("gageScatterplotMenuItem");
+    let hydroDataViewerBtn = document.getElementById("hydroDataViewerMenuItem");
+    let quickHydroBriefBtn = document.getElementById("quickHydroBriefMenuItem");
+    //menu2 sections
+    let menu2 = document.getElementById("menu2");
+    
+    let obsMonitor = document.getElementById("obsMonitor");
+    let gageScatterplot = document.getElementById("gageScatterplot");
+    let hydroDataViewer = document.getElementById("hydroDataViewer");
+    let quickHydroBrief = document.getElementById("quickHydroBrief");
+    
+    function hideMenu2() {menu2.style.display = "none";}
+    function showMenu2() { menu2.style.display = "block";}
+
+    hideAllContentContainers();
+    bio.style.display = "block";
+    hideAllSoftwareProjectContainers();
+    
+    aboutMeBtn
+	.addEventListener("click",
+			  function() {hideMenu2();
+				      hideAllContentContainers();
+				      bio.style.display = "block";
+				     });
+    projectsBtn
+	.addEventListener("click",
+			  function() {showMenu2();
+				      hideAllContentContainers();
+				      projects.style.display = "block";
+				     });
+    skillsBtn
+	.addEventListener("click",
+			  function() {hideMenu2();
+				      hideAllContentContainers();
+				      skills.style.display = "block";
+				     });
+
+    obsMonitorBtn
+	.addEventListener("click",
+			  function() {hideAllSoftwareProjectContainers();
+				      obsMonitor.style.display = "block";
+				     });
+
+    gageScatterplotBtn
+	.addEventListener("click",
+			  function() {hideAllSoftwareProjectContainers();
+				      gageScatterplot.style.display = "block";
+				     });
+
+    hydroDataViewerBtn
+	.addEventListener("click",
+			  function() {hideAllSoftwareProjectContainers();
+				      hydroDataViewer.style.display = "block";
+				     });    
+    quickHydroBriefBtn
+	.addEventListener("click",
+			  function() {hideAllSoftwareProjectContainers();
+				      quickHydroBrief.style.display = "block";
+				     });
+
+    
+}
+
+document.getElementById("menu2").style.display = "none";
+
+initializeMenus();
+
+/*
 function initializeVideos() {
     let michaelVideo = document.getElementById("michaelVideo");
     let florenceVideo = document.getElementById("florenceVideo");
@@ -24,3 +116,4 @@ function initializeVideos() {
 }
 
 initializeVideos();
+*/
